c0f1cfb7afcb4cf95fae4b851877c69fdcf97985
zopirov <folziii.yt@gmail.com>
2025-10-08 23:29:54 +0500

    Второй урок(Добовление системы монеток)

--- goblin.c
+++ goblin.c
@@ -3,7 +3,8 @@
 int main (void) { 
     
     int gold = 15;
-    int hp_goblin = 3;
+    int hp_goblin;
+    int player_gold = 0;
     char attack;
 
     printf("Атака гоблина. (Нажми на (E)-для атаки) \n(Нажми на (R)-чтобы убежать) \nЖизнь Гоблина: %d\n", hp_goblin);
@@ -12,28 +13,36 @@ int main (void) {
 
     while (attack != 'r')
     {
-            if (attack == 'E' || attack == 'e')
+        if (attack == 'E' || attack == 'e')
         {
-            hp_goblin = hp_goblin -1; 
-            printf("Пнул гоблина. \nЖизнь Гоблина: %d\n", hp_goblin);
+            for (hp_goblin = 3; hp_goblin >= 0; hp_goblin--)
+            {
+                if (hp_goblin != 3)
+                    printf("Пнул гоблина. \nЖизнь Гоблина: %d\n", hp_goblin);
+                     
+                if (hp_goblin == 0)
+                {
+                    printf("Красава! ты убил Гоблина. \n");
+                    printf("Получаешь награду %d монеток. Всего %d монеток на балансе!\n", gold, player_gold += gold);
+                    break;
+                }
+            }
         }
 
-     if (hp_goblin < 1)
-        {
-
-            printf("Красава! ты убил Гоблина. \n");
-            printf("Получаешь награду %d монеток. \n", gold);
-        }
      else
         {
+            if (attack == 'r')
             printf("Гоблин убежал - ты лох! \n");
         }   
-            printf("Чо дальше делать будешь? \n");
+            printf("Чо дальше делать будешь? \nНажми на (E)-для атаки \nНажми на (R)-чтобы убежать");
             getchar();
             attack = getchar();
     }
+        if (attack == 'r')
+        {
+        printf("куда бежишь? \n");
+        }
     
-    printf("куда бежишь? \n");
 
-    return 0;
+        return 0;
 }
