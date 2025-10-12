c4a0df59d3468a1b5b018060e7e9ff2c2f0173cf
irovbyte <folzi_@outlook.com>
2025-10-12 14:42:43 +0500

    ДУлучшение кода Улучшение укода (Функция)Ð

--- goblin.c
+++ goblin.c
@@ -1,48 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main (void) { 
-    
+int battle(int player_gold) {
     int gold = 15;
-    int hp_goblin;
-    int player_gold = 0;
+    int hp_goblin = 3;
     char attack;
-
-    printf("Атака гоблина. (Нажми на (E)-для атаки) \n(Нажми на (R)-чтобы убежать) \nЖизнь Гоблина: %d\n", hp_goblin);
-
-    attack = getchar();
-
-    while (attack != 'r')
-    {
-        if (attack == 'E' || attack == 'e')
-        {
-            for (hp_goblin = 3; hp_goblin >= 0; hp_goblin--)
-            {
-                if (hp_goblin != 3)
-                    printf("Пнул гоблина. \nЖизнь Гоблина: %d\n", hp_goblin);
-                     
-                if (hp_goblin == 0)
-                {
-                    printf("Красава! ты убил Гоблина. \n");
-                    printf("Получаешь награду %d монеток. Всего %d монеток на балансе!\n", gold, player_gold += gold);
-                    break;
-                }
+    while (hp_goblin > 0) {
+        while ((attack = getchar()) == '\n'); // ждём символ
+        if (attack == 'E' || attack == 'e') {
+            printf("Пнул гоблина. \nЖизнь Гоблина: %d\n", hp_goblin-1);
+            hp_goblin--;
+        } else if (attack == 'R' || attack == 'r') {
+            int chance = rand() % 2;
+            if (chance == 0) {
+                return player_gold;
+            } else {
+                printf("Гоблин не напуган! Продолжаем бой.\n");
             }
+        } else {
+            printf("Некорректный ввод!\n");
         }
-
-     else
-        {
-            if (attack == 'r')
-            printf("Гоблин убежал - ты лох! \n");
-        }   
-            printf("Чо дальше делать будешь? \nНажми на (E)-для атаки \nНажми на (R)-чтобы убежать");
-            getchar();
-            attack = getchar();
     }
-        if (attack == 'r')
-        {
-        printf("куда бежишь? \n");
-        }
-    
+    printf("Красава! ты убил Гоблина. \n");
+    printf("Получаешь награду %d монеток. Всего %d монеток на балансе!\n", gold, player_gold + gold);
+    player_gold += gold;
+    return player_gold;
+}
 
-        return 0;
+int hello(char *nik)
+
+{
+    char c;
+
+    printf("Как тебя звать?\n");
+
+    scanf("%s", nik);
+
+    while ((c = getchar()) != '\n' && c != EOF); //чистка буфера 
+
+    printf("Чо? А тебя зовут - %s?\n", nik);
+
+    printf("%s! Приятной тебе игры!\n", nik);
+    
+    return 0;
+}
+
+int main(void)
+{
+    char nik[20];
+    int player_gold = 0;
+    hello(nik);
+    while (1) {
+        printf("Атака гоблина. \n(Нажми на (E)-для атаки) \n(Нажми на (R)-чтобы напугать Гоблина) \nЖизнь Гоблина: 3\n");
+        int old_gold = player_gold;
+        player_gold = battle(player_gold);
+        if (player_gold == old_gold) {
+            printf("Гоблин сбежал! Новый гоблин выходит на бой!\n");
+        } else {
+            printf("Ты победил гоблина! Новый гоблин выходит на бой!\n");
+        }
+        printf("Всего монеток на балансе: %d\n", player_gold);
+        printf("Хочешь продолжить игру? (Y - да, N - нет)\n");
+        char cont;
+        while ((cont = getchar()) == '\n');
+        if (cont == 'N' || cont == 'n') break;
+    }
+    printf("Игра окончена! Всего монеток на балансе: %d\n", player_gold);
+    return 0;
 }
